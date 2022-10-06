54640aa9a7982d5b80cd26180679560e0c1fd20c
Namish Kumar <namishkumar000@gmail.com>
2022-10-06 13:04:37 +0530

    Added writing support

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 const NamishDB = "NamishDB";
 const yargs = require("yargs");
+const axios = require("axios");
 const fs = require("fs");
+const cors = require("cors");
 const dns = require('dns');
 const { toNamespacedPath } = require("path");
 const { DefaultDeserializer } = require("v8");
@@ -117,6 +119,39 @@ if(mainArgue === "start") {
                 res.end("300");
             }
        });
+
+       app.post("/checkIfExists", function(req,res) {
+            const body = req.body;
+            const document = body.document;
+            const collection = body.collection;
+            const password = body.password;
+            const data = body.data;
+            if(password === serverPassword) {
+                const path = "databaseSource/src/" + collection + "/" + document + "/";
+                if (fs.existsSync(path)) {
+                    res.json({type:"error", message:"NamishDB Server error: The document and collection already exists! Please try again!"});
+                }else{
+                    fs.writeFile(path + "data.json", data, (err) => {
+                        if (err) {
+                          console.log("NamishDB Error: Unable to write to database <NamishDB.Error.UnableToWriteToDB>");
+                          res.json({type:"error", message:"NamishDB Error: Unable to write to database <NamishDB.Error.UnableToWriteToDB>"});
+                        } else {
+                           console.log("NamishDB Process: Successfully written your document");
+                           res.json({
+                             type:"success",
+                             document:document,
+                             collection:collection,
+                             dbReference: path,
+                             by: NamishDB,
+                             message: "Successfully added your data to the Database. Kindly proceed",               
+                            });
+                        }
+                      });
+                }
+            }else{
+                res.json({type:"error", message:"NamishDB Authentication Error: Invalid Password received! Please try again!"});
+            }
+       });
        app.get("/sample", function(req, res){
         res.end(`
         <!DOCTYPE html>
@@ -226,31 +261,66 @@ if(mainArgue === "start") {
       const document = req.body.document;
       const collection = req.body.collection;
       const data = req.body.data;
-      if(collection === null || collection === undefined || document === null || document === undefined || collection === "" || document === "") {
-         res.json({
-            type:"error",
-            why:"Invalid Database reference",
-            message: "Invalid Database reference",
-            exp:"Invalid collection or document name"
-         });
-      }else{
-        const path = "databaseSource/src/" + collection + "/" + document + "/";
-        fs.writeFile(path + "data.json", data, (err) => {
-            if (err)
-              console.log("NamishDB Error: Unable to write to database <NamishDB.Error.UnableToWriteToDB>");
-            else {
-               console.log("NamishDB Process: Successfully written your document");
-               res.json({
-                 type:"success",
-                 document:document,
-                 collection:collection,
-                 dbReference: path,
-                 by: NamishDB,
-                 message: "Successfully added your data to the Database. Kindly proceed",               
-                });
+      console.log("NamishDB Process: Connection request from IP " + req.ip);
+      if(password === serverPassword) {
+        if(collection === null || collection === undefined || document === null || document === undefined || collection === "" || document === "") {
+            res.json({
+               type:"error",
+               why:"Invalid Database reference",
+               message: "Invalid Database reference",
+               exp:"Invalid collection or document name"
+            });
+         }else{
+           const path = "databaseSource/src/" + collection + "/" + document + "/";
+           writeFile(path, data, function (err) {
+            if(err) {
+                console.error(err)
             }
-          });
+            res.json({
+                type:"success",
+                document:document,
+                collection:collection,
+                dbReference: path,
+                by: NamishDB,
+                message: "Successfully added your data to the Database. Kindly proceed",               
+               });
+           })
+           function writeFile(path, data, cb) {
+            fs.mkdir(path, { recursive: true}, function (err) {
+              if (err) return cb(err);
+              fs.writeFile(path + "data.json", data, (err) => {
+                if (err) {
+                 console.log(err);
+                  console.log("NamishDB Error: Unable to write to database <NamishDB.Error.UnableToWriteToDB>");
+                 }else {
+                   console.log("NamishDB Process: Successfully written your document");
+                }
+              });
+            });
+          }
+         }
+      }else{
+        res.json({type:"error", message:"Invalid DB password!"});
       }
+   });
+   // Make a post request to '<your_ip>/delete' to delete data from the DB
+   app.post("/delete", function(req, res) {
+        const body = req.body;
+        console.log("NamishDB Process: Connection request from IP address " + req.ip);
+        const password = body.password;
+        const document = body.document;
+        const collection = body.collection;
+        const path = "databaseSource/src/" + collection + "/" + document + "/";
+        if(password === serverPassword) {
+            fs.unlink(path + "data.json", (err) => {
+                if (err) {
+                    res.json({type:"error", message:"Invalid Database Reference"});
+                };
+                res.json({type:"success", message:"The data was successfully removed from NamishDB"});
+              });
+        }else{
+            res.json({type:"error", message:"NamishDB Client error: Invalid password. (The password is wrong.)"});
+        }
    });
         }else if(serverMode === "production") {
             readline.close();
@@ -429,9 +499,10 @@ app.post("/write", function (req, res){
    }else{
      const path = "databaseSource/src/" + collection + "/" + document + "/";
      fs.writeFile(path + "data.json", data, (err) => {
-         if (err)
+         if (err) {
+           console.error(err);
            console.log("NamishDB Error: Unable to write to database <NamishDB.Error.UnableToWriteToDB>");
-         else {
+         } else {
             console.log("NamishDB Process: Successfully written your document");
             res.json({
               type:"success",
@@ -445,6 +516,24 @@ app.post("/write", function (req, res){
        });
    }
 });
+// Make a post request to <your_ip>/delete to delete data from the DB
+app.post("/delete", function(req, res) {
+    const body = req.body;
+    const password = body.password;
+    const document = body.document;
+    const collection = body.collection;
+    const path = "databaseSource/src/" + collection + "/" + document + "/";
+    if(password === serverPassword) {
+        fs.unlink(path + "data.json", (err) => {
+            if (err) {
+                res.json({type:"error", message:"Invalid Database Reference"});
+            };
+            res.json({type:"success", message:"The data was successfully removed from NamishDB"});
+          });
+    }else{
+        res.json({type:"error", message:"NamishDB Client error: Invalid password. (The password is wrong.)"});
+    }
+});
         }else{
             return console.log("\nNamishDB Error: No such server mode. Please enter a valid server mode. Error : <NamishDB.Errors.NoSuchConfigError>");
         }
@@ -454,8 +543,188 @@ app.post("/write", function (req, res){
     });
 }
 if(mainArgue === "connect") {
-       
-}
+       let globalIP;
+       let globalPass;
+       console.log("NamishDB Client: Welcome to NamishDB Client Connector.");
+       readline.question('\nPlease enter the host name or IP address of the NamishDB server **including HTTP or HTTPS** \nEnter here: ', serverEnteredIP => {
+            continueByPassword(serverEnteredIP); 
+       });
+
+       function continueByPassword(serverEnteredIP) {
+          console.log("\nNamishDB Client: Received the IP address of the DB server as " + serverEnteredIP);
+          readline.question('\nPlease enter the password of the NamishDB server\nEnter here: ', serverEnteredPass => {
+                 connectToServer(serverEnteredIP, serverEnteredPass);
+          });
+       }
+
+       function connectToServer(serverEnteredIP, serverEnteredPass) {
+          console.log("\nNamishDB Client: Received the password as " + serverEnteredPass);
+          console.log("\nNamishDB Connector: Trying to connect to " + serverEnteredIP + " . Please wait for a moment...");
+          axios.post(serverEnteredIP + "/verify", {
+              password:serverEnteredPass,
+          })
+          .then((response) => {
+             continueConnection(response.data, serverEnteredIP, serverEnteredPass);
+          }, (error) => {
+            if(String(error) === "TypeError [ERR_INVALID_URL]: Invalid URL") {
+                console.log("NamishDB Connector: Invalid NamishDB URL! Error: <NamishDB.ClientError.InvalidURL>. For Error logs, kindly have a look at the NamishDB_client_error.log file")
+                exit(1);
+            }
+
+            if(String(error) === "Error: getaddrinfo ENOTFOUND verify" || String(error) === "Error: getaddrinfo ENOTFOUND .") {
+                console.log("NamishDB Connector: Unable to get information about the IP address. Error: <NamishDB.ClientError.ENOTFOUND>. For Error logs, kindly have a look at the NamishDB_client_error.log file");
+            }
+            
+            fs.writeFile("NamishDB_client_error.log", "NamishDB Client Error: [ Latest Time (Now) ]\nNodeJS Error (Error.Type.Internal <Maybe Client or Server-side>)\n" + error, (err) => {
+                if (err) throw err;
+            });
+            console.log(error);
+          });
+       }
+
+       function continueConnection(mainRes, serverEnteredIP, serverEnteredPass) {
+        globalIP = serverEnteredIP;
+        globalPass = serverEnteredPass;
+          if(String(mainRes) === "400") {
+            console.log("NamishDB Client: Connected to " + serverEnteredIP + " successfully. Go to https://db.namishkumar.in/db/client.html for known commands.");
+            askCommand(String(mainRes), serverEnteredIP, serverEnteredPass);
+          }else{
+            console.log("NamishDB Client: Failed to connect to NamishDB server. This may be probably because of invalid password. Please try again!");
+            exit(1);
+          }
+       }
+
+       function askCommand(mainRes, serverEnteredIP, serverEnteredPass) {
+          readline.question('\nThese are the options. Press:- \n1. To add some data\n2. To delete some data\n3. To stop the server\n4. To restart the server\n5. To disconnect from the server\nEnter here: ', pressedMenu => {
+              if(pressedMenu === undefined || pressedMenu === null || pressedMenu === Infinity || pressedMenu === NaN || pressedMenu.length === 0 || pressedMenu === "" || pressedMenu === 0) {
+                 console.log("\nSorry, Invalid Command received. Please try again!");
+                 askCommand(mainRes, serverEnteredIP, serverEnteredPass);
+              }else{
+                  reactToCommand(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu);
+              }
+          });
+       }
+
+       function reactToCommand(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu) {
+             if(pressedMenu === "1") {
+                  console.log("\nTrying to reconnect to " + serverEnteredIP);
+                  askCollection(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu);
+             }
+       }
+
+       function askCollection(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu) {
+        readline.question('\nPlease enter the name of the collection.\nEnter here: ', serverEnteredCollection => { 
+             if(serverEnteredCollection === null || serverEnteredCollection === undefined || serverEnteredCollection === NaN || serverEnteredCollection === "" || serverEnteredCollection.length === 0) {
+                 console.log("\nInvalid Collection name. Please try again!");
+                 askCollection(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu);
+             }else{
+                 console.log(`\nNamishDB Client: Received the collection name as ${serverEnteredCollection}`);
+                 askDocument(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection);
+             }
+        });
+       }
+
+       function askDocument(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection) {
+        readline.question('\nPlease enter the name of the document.\nEnter here: ', serverEnteredDocument => {
+            if(serverEnteredDocument === null || serverEnteredDocument === undefined || serverEnteredDocument === NaN || serverEnteredDocument === "" || serverEnteredDocument.length === 0) {
+                console.log("\nInvalid Document name. Please try again!");
+                askDocument(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection);
+            }else{
+                console.log(`\nNamishDB Client: Received the Document name as ${serverEnteredDocument}`);
+                console.log("\nNamishDB Client Connector Process: Initialising Data service...");
+                askForData(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection, serverEnteredDocument)
+            }
+        });
+       }
+
+       function askForData(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection, serverEnteredDocument) {
+        readline.question('\nPlease choose the required option\n1. To add data from a JSON file\n2. To add data by regular method\nEnter here: ', opType => {
+            if(opType === undefined || opType === null || opType === NaN || opType === Infinity || opType === "" || opType.length === 0) {
+                console.log("\nInvalid Input received! Please try again!");
+                askForData(mainRes, serverEnteredIP, serverEnteredPass, pressedMenu, serverEnteredCollection, serverEnteredDocument);
+            }else{
+                if(opType === "2") {
+                    console.log("\nNamishDB: Please wait...");
+                    askForDataByRegularMethod(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument);
+                }
+            }
+        });
+     }
+      
+     let noOfDataNow = 0;
+     let noOfMainData = 0;
+     let dataInOBJ = {};
+     function askForDataByRegularMethod(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument) {
+        readline.question('\nHow many data do you want to add in this DB reference?\nEnter here: ', noOfDataToBeAdded => { 
+            if(noOfDataToBeAdded === undefined || noOfDataToBeAdded === NaN || noOfDataToBeAdded === null || noOfDataToBeAdded === Infinity || noOfDataToBeAdded === "") {
+                console.error("Sorry, Expected a valid number but got null! Please try again!");
+                askForDataByRegularMethod(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument);
+            }else{
+                const toAddData = Number(noOfDataToBeAdded);
+                noOfMainData += toAddData;
+                console.log(`A total of ${noOfMainData} data/properties can be added as per your instructions`);
+                askForMainDataNow(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument);
+            }
+        }); 
+     }
+     
+     function askForMainDataNow(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument) {
+        readline.question('\nEnter the name of the data\nEnter here: ', mainDataProperty => { 
+            if(mainDataProperty === undefined || mainDataProperty === NaN || mainDataProperty === null || mainDataProperty === Infinity || mainDataProperty === "") {
+                console.error("Sorry, Invalid data received! Please try again!");
+                askForMainDataNow(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument);
+            }else{
+                askForDataValue(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument, mainDataProperty);
+            }
+        });
+     }
+
+     function askForDataValue(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument, mainDataProperty) {
+        readline.question('\nEnter the value/string of the data\nEnter here: ', mainDataValue => { 
+            if(mainDataValue === undefined || mainDataValue === NaN || mainDataValue === null || mainDataValue === Infinity || mainDataValue === "") {
+                console.error("Sorry, Invalid data received! Please try again!");
+                askForDataValue(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument, mainDataProperty);
+            }else{
+                tryToAddTheData(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument, mainDataProperty, mainDataValue);
+            }
+        });
+     }
+
+     function tryToAddTheData(serverEnteredIP, serverEnteredPass, serverEnteredCollection, serverEnteredDocument, mainDataProperty, mainDataValue) {
+          if(noOfMainData === 0 || noOfDataNow === 0 || noOfDataNow <= (noOfMainData - 1)) {
+              dataInOBJ += {
+                 [mainDataProperty] : mainDataProperty
+              };
+              noOfDataNow ++;
+              console.log(`\n${noOfDataNow} has been added till now. Asking more data...`);
+              askForMainDataNow(serverEnteredPass, serverEnteredCollection, serverEnteredDocument);
+          }else if(noOfDataNow === noOfMainData) {
+              console.log("\nYour data will look like this:- ");
+              console.log("\n");
+              console.log(JSON.stringify(dataInOBJ));
+              console.log(`\nRequesting the server to add all the data/properties`);
+              console.log("\nConnecting " + globalIP);
+              axios.post(globalIP + "/write", {
+                password:globalPass,
+                document: serverEnteredPass,
+                collection: serverEnteredIP,
+                data: dataInOBJ,
+            })
+            .then((response) => {
+                if(response.data.type === 'error') {
+                    console.error("\nNamishDB Error: " + response.data.message);
+                    exit(1);
+                }else{
+                    console.log(response.data.message);
+                }
+             }, (error) => {
+                console.error(error);
+             });
+             
+          }
+     }
+        
+        }
 
 if(mainArgue === "setup") {
     const configObj = {
